8b03fb34c25df9b5bc0cae2e8ec2e83cdd46f670
MJ Rossetti <s2t2@users.noreply.github.com>
2022-07-21 16:46:54 -0400

    Incorporate fixed logic into the game

--- game.py
+++ game.py
@@ -64,23 +64,5 @@ if __name__ == "__main__":
     # DETERMINATION OF WINNER
     #
 
-    if u == "rock" and c == "rock":
-        print("It's a tie!")
-    elif u == "rock" and c == "paper":
-        print("The computer wins")
-    elif u == "rock" and c == "scissors":
-        print("The user wins")
-
-    elif u == "paper" and c == "rock":
-        print("The computer wins")
-    elif u == "paper" and c == "paper":
-        print("It's a tie!")
-    elif u == "paper" and c == "scissors":
-        print("The user wins")
-
-    elif u == "scissors" and c == "rock":
-        print("The computer wins")
-    elif u == "scissors" and c == "paper":
-        print("The user wins")
-    elif u == "scissors" and c == "scissors":
-        print("It's a tie!")
+    winning_message = winner(user_choice=u, computer_choice=c)
+    print(winning_message)
